07cf8323e9999d4610ff278c68df64224d0506f8
IfalEXZ <ifaltampan69@gmail.com>
2024-12-05 23:08:12 +0800

    fix spacing in Home component layout for better visual consistency

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GitHubButton from "./components/GithubButton";
 export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-center p-24 bg-pink-300">
-      <div className="flex flex-row items-center justify-center gap-4 m-10">
+      <div className="flex flex-row items-center justify-center gap-4 mb-10">
         <h1 className="text-2xl font-bold w-64 h-16 flex items-center justify-center text-black bg-white border-4 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]">
           Snake Game
         </h1>
